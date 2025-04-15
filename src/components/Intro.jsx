78706be64f860a6068ae5e294fe6082a893d117b
dillonhoh <67936878+dillonhoh@users.noreply.github.com>
2025-04-14 22:00:01 -0700

    changed StartPicture and more styling adjustments

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,13 +2,14 @@ export default function Intro() {
 
     return (
         <div className="title">
-            <h1 className="text-4xl text-[#E6D8C7] font-semibold">About Me</h1>
+            <h1 className="text-4xl text-[#E6D8C7] font-semibold text-center">About Me</h1>
+            <p className="text-lg text-[#E6D8C7] font-semibold">Driven student pursuing Computer Science and Business @ UCR </p>
             <div className="flex justify-between text-base text-[#E6D8C7] font-semibold">
             <h1>(626) 695 - 8683</h1>
             <h1>dhoh001@ucr.edu</h1>
             </div>
 
-            <p className="text-lg text-[#E6D8C7] font-semibold">Driven student pursuing Computer Science and Business @ UCR </p>
+            
         </div>
     )
 }
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -4,23 +4,23 @@ export default function Start() {
 
     const textimageArray = [
         {text: "Software Engineer", 
-        image: "../src/assets/softwareengineer.jpg"
+        image: "../src/assets/swecircle.png"
         }, 
         {text: "Tailor",
         image: "../src/assets/photographer.jpg"
         }, 
         {text: "Dancer", 
-        image: "../src/assets/dancer.jpg"
+        image: "../src/assets/dancercircle.png"
         },
         {text: "Snowboarder", 
-        image: "../src/assets/snowboarder.jpg"
+        image: "../src/assets/snowboardercircle.png"
         },
         ];
 
 
 
 const [selectedIndex, setSelectedIndex] = useState(5)
-const [styleChose, setStyleChosen] = useState(false)
+const [styleChosen, setStyleChosen] = useState(false)
 
 const handleClick = (clickedIndex) => {
     if (clickedIndex == selectedIndex) {
@@ -28,35 +28,41 @@ const handleClick = (clickedIndex) => {
         setSelectedIndex(5);
         return
     }
+    setStyleChosen(true);
     setSelectedIndex(clickedIndex);
 }
 
     return(
         <>
-            <div className="flex items-center mt-[125px] w-6xl justify-between">
+            <div className="flex items-center mt-[125px] w-6xl justify-evenly caret-transparent ml-[-95px]">
                 <div className="flex" >
                     <img 
                     key={selectedIndex}
-                    src={textimageArray[selectedIndex] ? textimageArray[selectedIndex].image : "../src/assets/dillon.jpg"} 
+                    src={textimageArray[selectedIndex] ? textimageArray[selectedIndex].image : "../src/assets/dilloncircle.png"} 
                     alt="startPhoto"
-                    className={`start-picture h-[375px] ${ textimageArray[selectedIndex] ? 'active-picture' : ''} opacity-95`}/>
+                    className={`start-picture h-[425px] ${ textimageArray[selectedIndex] ? 'active-picture' : ''} opacity-90`}/>
                 </div>
 
                 <div className="start-text flex-col text-right">
                     <div className="text-6xl text-[#E6D8C7] font-semibold">Hey! I'm Dillon Ho</div>
-                    <div className="text-base text-[#E6D8C7] text-center">thanks for checking this out</div>
+                    <div className="text-base text-[#E6D8C7] text-center font-light">thanks for checking this out</div>
                     <hr className="my-1 border-[#E6D8C7] border-t-2" />
                     <hr className="my-1 border-[#E6D8C7] border-t-2" />
+                    <div className="styles-container">
+                    <hr className="my-1 border-[transparent" />
                     {textimageArray.map((item, index) => (
                         <div
                             key={index}
-                            className={`text-item ${selectedIndex === index ? 'active' : ''} text-[#E6D8C7] text-xl font-semibold opacity-[50%]`}
+                            className={`text-item ${selectedIndex === index ? 'active' : ''} text-[#E6D8C7] text-xl font-semibold opacity-[50%] cursor-pointer mt-[3px]`}
                             onClick = {() => handleClick(index)}
                             >
                             {item.text}
                         </div>
                     ))}
+                    </div>
+                    <div className={`${styleChosen == true ? 'blinkingStopped' : 'blinkingActive'} text-center text-sm `}>select a style</div>
                 </div>
+
             </div>
         </>
 
